555adf2b29bcb30a9bcb9fbcce68809e63f1cef4
Matthieu Riegler <kyro38@gmail.com>
2024-04-19 22:59:12 +0200

    minimal repro for https://github.com/angular/angular/issues/55396

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,37 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { APP_ID, APP_INITIALIZER, ApplicationConfig } from '@angular/core';
 
-import { routes } from './app.routes';
+import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
 import { provideClientHydration } from '@angular/platform-browser';
+import { map } from 'rxjs';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideHttpClient(withFetch()),
+    provideClientHydration(),
+    { provide: APP_ID, useValue: 'serverApp' },
+    {
+      provide: APP_INITIALIZER,
+      multi: true,
+      useFactory: (httpClient: HttpClient) => () => {
+        return httpClient
+          .get(
+            'https://upload.wikimedia.org/wikipedia/commons/e/e3/Owned_by_USA_and_Canada_East_Quoddy_Lighthouse%2C_located_on_Campobello_Island.jpg',
+            { responseType: 'blob' }
+          )
+          .pipe(map(() => randomString(10000)));
+      },
+      deps: [HttpClient],
+    },
+  ],
 };
+
+const alphabet =
+  '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+function randomString(size: number): string {
+  let r = '';
+  for (let i = 0; i < size; i++) {
+    r += alphabet[Math.floor(Math.random() * alphabet.length)];
+  }
+  return r;
+}
